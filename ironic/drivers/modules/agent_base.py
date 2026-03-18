JohnGarbutt/ironic

commit d2cd289bc1181ff088edf5c1269800ed8c7776ab

fix: Remove early excessive return
An early return was short circuiting handling on an attempt
for the agent to refresh steps mid-sequence by just returning
instead of logging or raising an exception when the issue
was encountered.

This issue was discovered by a code scanner, and upon human
research, it is indeed a minor bug although not known to be
encountered by a human.

Assisted-By: Claude Code - Claude Sonnet 4.5
Change-Id: I38079502bf5a107820366f57f1545b216554b6ad
Signed-off-by: Julia Kreger <juliaashleykreger@gmail.com>

ironic/drivers/modules/agent_base.py
@@ -943,8 +943,6 @@ class AgentBaseMixin(object):
             LOG.debug('Agent for node %(node)s is busy with a command, '
                       'will refresh steps on the next heartbeat.',
                       {'node': task.node.uuid})
-            return
-
             # TODO(dtantsur): change to just 'raise'
             if step_type == 'clean':
                 raise

ironic/tests/unit/drivers/modules/test_agent_base.py
@@ -1497,7 +1497,11 @@ class TestRefreshCleanSteps(AgentDeployMixinBaseTest):
                              task.node.driver_internal_info)
             self.assertIsNone(task.node.driver_internal_info.get(
                 'agent_cached_deploy_steps'))
-            log_mock.assert_not_called()
+            log_mock.assert_called_once_with(
+                'Agent running on node %(node)s does not support '
+                'in-band deploy steps: %(err)s. Support for old '
+                'agents will be removed in the V release.',
+                {'node': task.node.uuid, 'err': mock.ANY})
 
     @mock.patch.object(agent_client.AgentClient, 'get_clean_steps',
                        autospec=True)

commit 50bf1142eed5ad1a5c962d0270d146f2867cccdd

Do not raise in refresh_steps if the agent is busy during cleaning
Regression in d2cd289bc1181ff088edf5c1269800ed8c7776ab.
The intention of the code was to catch two different conditions, not to
mangle them into one. Since the compatibility code was supposed to be
removed in V, just remove it now.

Change-Id: Ibd66018a68082d75d2e7e793a7c281408d1168e7
Signed-off-by: Dmitry Tantsur <dtantsur@protonmail.com>

ironic/drivers/modules/agent_base.py
@@ -939,19 +939,11 @@ class AgentBaseMixin(object):
         call = getattr(client, 'get_%s_steps' % step_type)
         try:
             agent_result = call(node, task.ports).get('command_result', {})
-        except exception.AgentInProgress as exc:
+        except exception.AgentInProgress:
             LOG.debug('Agent for node %(node)s is busy with a command, '
                       'will refresh steps on the next heartbeat.',
                       {'node': task.node.uuid})
-            # TODO(dtantsur): change to just 'raise'
-            if step_type == 'clean':
-                raise
-            else:
-                LOG.warning('Agent running on node %(node)s does not support '
-                            'in-band deploy steps: %(err)s. Support for old '
-                            'agents will be removed in the V release.',
-                            {'node': node.uuid, 'err': exc})
-                return
+            return
 
         missing = set(['%s_steps' % step_type,
                        'hardware_manager_version']).difference(agent_result)

releasenotes/notes/agent-without-deploy-steps-83bf64e9bea618da.yaml
@@ -1,0 +1,5 @@
+---
+upgrade:
+  - |
+    Removes compatibility with Ironic Python Agent versions without support
+    for in-band deploy steps (introduced in the Ussuri cycle).

ironic/tests/unit/drivers/modules/test_agent_base.py
@@ -1497,11 +1497,6 @@ class TestRefreshCleanSteps(AgentDeployMixinBaseTest):
                              task.node.driver_internal_info)
             self.assertIsNone(task.node.driver_internal_info.get(
                 'agent_cached_deploy_steps'))
-            log_mock.assert_called_once_with(
-                'Agent running on node %(node)s does not support '
-                'in-band deploy steps: %(err)s. Support for old '
-                'agents will be removed in the V release.',
-                {'node': task.node.uuid, 'err': mock.ANY})
 
     @mock.patch.object(agent_client.AgentClient, 'get_clean_steps',
                        autospec=True)